e0ba6c52e1bf0ac16ca58208198f0c70e18aa8b4
xinluz6 <156732707+xinluz6@users.noreply.github.com>
2024-05-22 16:28:17 -0700

    add csrf error

--- bank/app.py
+++ bank/app.py
@@ -13,21 +13,23 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         SQLALCHEMY_DATABASE_URI='sqlite:///' +
         os.path.join(app.instance_path, 'database.db'),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
-
+    csrf = CSRFProtect(app)
     db.init_app(app)
     app.register_blueprint(bp_transaction, url_prefix="")
     app.register_blueprint(bp_user, url_prefix="")
-    csrf = CSRFProtect(app)
+
+    @app.errorhandler(400)
+    def handle_csrf_error(e):
+        return render_template('user/csrf_error.html', error='The CSRF token is invalid.'), 400
 
     return app
-
-
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
