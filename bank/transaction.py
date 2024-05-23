267991a6943bea49e482fcc70fad1fad7313e4b0
sscimff <wtc9199@gmail.com>
2024-05-23 12:49:39 -0700

    Add "download file" feature

--- bank/transaction.py
+++ bank/transaction.py
@@ -1,9 +1,18 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, session, g
+import io
+from datetime import datetime
+import re
+from flask import Blueprint, render_template, flash, redirect, request, send_file, url_for, session, g
 from db import Account, db
 from transaction_form import DepositForm, WithdrawForm
+from flask_wtf import FlaskForm
+
 
 bp_transaction = Blueprint("transaction", __name__,
                            template_folder="templates/transaction")
+
+
+class DummyForm(FlaskForm):
+    pass
 
 
 @bp_transaction.before_request
@@ -30,7 +39,8 @@ def show():
         return redirect(url_for('user.login'))
 
     account = g.user
-    return render_template("show_balance.html", account=account)
+    dummy_form = DummyForm()
+    return render_template("show_balance.html", account=account, form=dummy_form)
 
 
 @bp_transaction.route('/deposit', methods=['GET', 'POST'])
@@ -70,3 +80,33 @@ def withdraw():
         else:
             flash('Account not found.', 'danger')
     return render_template('withdraw.html', form=form, account=account)
+
+
+@bp_transaction.route('/save_transaction', methods=['POST'])
+def save_transaction():
+    if g.user is None:
+        return redirect(url_for('user.login'))
+
+    filename = request.form.get('filename')
+
+    safe_filename = re.sub(r'[^a-zA-Z0-9_\-]', '_', filename)
+    safe_filename = f"{safe_filename}_{
+        datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
+
+    data = f"Current Balance: ${g.user.balance:.2f}"
+
+    try:
+        file_content = io.BytesIO()
+        file_content.write(data.encode('utf-8'))
+        file_content.seek(0)
+
+        return send_file(
+            file_content,
+            as_attachment=True,
+            download_name=filename,
+            mimetype='text/plain'
+        )
+    except Exception as e:
+        flash(f'Error saving transaction history: {str(e)}', 'danger')
+
+    return redirect(url_for('transaction.show'))
